aebb98b63e8a79e637253fbe40d82dbdb83f1f07
Pablo Ridolfi <pablo.ridolfi@satellogic.com>
2022-10-03 13:00:46 +0200

    trilateration working

--- src/main.rs
+++ src/main.rs
@@ -89,27 +89,39 @@ fn play_with_nalgebra() {
 }
 
 fn trilaterate(references: &[Point3<f64>], distances: &[f64]) -> Point3<f64> {
-
+    let n = references.len()-1;
     assert_eq!(references.len(), distances.len());
-    let mut dist2ref0 = vec![0.; references.len()-1];
+    let mut dist2ref0 = vec![0.; n];
     for (i, v) in dist2ref0.iter_mut().enumerate() {
         *v = distance(&references[i+1], &references[0]);
     }
-    print!("dist2ref0 {:?}", dist2ref0);
-    
-    
-    let matrixA = DMatrix::<f64>::zeros(references.len()-1, 3);
+    println!("dist2ref0 {:?}", dist2ref0);
 
-    
-    
-    
-    Point3::new(0., 0., 0.)
+    let mut a = DMatrix::<f64>::zeros(n, 3);
+    for (i, mut row) in a.row_iter_mut().enumerate() {
+        row[0] = references[i+1].x - references[0].x;
+        row[1] = references[i+1].y - references[0].y;
+        row[2] = references[i+1].z - references[0].z;
+    }
+    println!("a {}", a);
+
+    let mut b = DMatrix::<f64>::zeros(n, 1);
+    for (i, mut row) in b.row_iter_mut().enumerate() {
+        row[0] = (distances[0].powi(2) - distances[i+1].powi(2) + dist2ref0[i].powi(2))/2.;
+    }
+    println!("b {}", b);
+
+    let a_t = a.transpose();
+    let x = (a.transpose() * a).try_inverse().unwrap() * a_t * b;
+    println!("x {}", x);
+
+    Point3::new(x[0]+references[0].x, x[1]+references[0].y, x[2]+references[0].z)
 }
 
 fn main() {
     signal_dbm_from_networks_scan();
     play_with_nalgebra();
-    let p = Point3::new(1.5,1.5,1.5);
+    let p = Point3::new(-1.,1.3,-3.);
     let mut references: Vec<Point3<f64>> = Vec::new();
     references.push(Point3::new(0.,0.,0.));
     references.push(Point3::new(3.,0.,0.));
@@ -123,6 +135,6 @@ fn main() {
     for (i, d) in distances.iter_mut().enumerate() {
         *d = distance(&references[i], &p);
     }
-    println!("{:?}", distances);
-    println!("{:?}", trilaterate(&references, &distances));
+    println!("distances {:?}", distances);
+    println!("trilat {:?}", trilaterate(&references, &distances));
 }
