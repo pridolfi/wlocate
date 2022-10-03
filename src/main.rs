a28dc1969a52f3a3907901fcd31c78f07e742c06
Pablo Ridolfi <pabloridolfi@gmail.com>
2022-10-03 19:19:07 +0200

    style

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{process::Command};
+use std::process::Command;
 use std::str;
 
-use nalgebra::{distance, Point3, DMatrix};
+use nalgebra::{distance, DMatrix, Point3};
 
 fn signal_dbm_to_distance_m(dbm: f64, freq_mhz: f64) -> f64 {
     let fspl = 27.55f64; // Free-Space Path Loss adapted avarage constant for home WiFI routers and following units
@@ -72,39 +72,43 @@ fn signal_dbm_from_networks_scan() {
 }
 
 fn trilaterate(references: &[Point3<f64>], distances: &[f64]) -> Point3<f64> {
-    let n = references.len()-1;
+    let n = references.len() - 1;
     assert_eq!(references.len(), distances.len());
     let mut dist2ref0 = vec![0.; n];
     for (i, v) in dist2ref0.iter_mut().enumerate() {
-        *v = distance(&references[i+1], &references[0]);
+        *v = distance(&references[i + 1], &references[0]);
     }
     let mut a = DMatrix::<f64>::zeros(n, 3);
     for (i, mut row) in a.row_iter_mut().enumerate() {
-        row[0] = references[i+1].x - references[0].x;
-        row[1] = references[i+1].y - references[0].y;
-        row[2] = references[i+1].z - references[0].z;
+        row[0] = references[i + 1].x - references[0].x;
+        row[1] = references[i + 1].y - references[0].y;
+        row[2] = references[i + 1].z - references[0].z;
     }
     let mut b = DMatrix::<f64>::zeros(n, 1);
     for (i, mut row) in b.row_iter_mut().enumerate() {
-        row[0] = (distances[0].powi(2) - distances[i+1].powi(2) + dist2ref0[i].powi(2))/2.;
+        row[0] = (distances[0].powi(2) - distances[i + 1].powi(2) + dist2ref0[i].powi(2)) / 2.;
     }
     let a_t = a.transpose();
     let x = (a_t.clone() * a).try_inverse().unwrap() * a_t * b;
-    Point3::new(x[0]+references[0].x, x[1]+references[0].y, x[2]+references[0].z)
+    Point3::new(
+        x[0] + references[0].x,
+        x[1] + references[0].y,
+        x[2] + references[0].z,
+    )
 }
 
 #[test]
 fn test_trilaterate() {
-    let p = Point3::new(-1.,1.3,3.);
+    let p = Point3::new(-1., 1.3, 3.);
     let references: Vec<Point3<f64>> = Vec::from([
-        Point3::new(0.,0.,0.),
-        Point3::new(3.,0.,0.),
-        Point3::new(0.,3.,0.),
-        Point3::new(3.,3.,0.),
-        Point3::new(0.,0.,3.),
-        Point3::new(3.,0.,3.),
-        Point3::new(0.,3.,3.),
-        Point3::new(3.,3.,3.)
+        Point3::new(0., 0., 0.),
+        Point3::new(3., 0., 0.),
+        Point3::new(0., 3., 0.),
+        Point3::new(3., 3., 0.),
+        Point3::new(0., 0., 3.),
+        Point3::new(3., 0., 3.),
+        Point3::new(0., 3., 3.),
+        Point3::new(3., 3., 3.),
     ]);
     let mut distances = vec![0.; references.len()];
     for (i, d) in distances.iter_mut().enumerate() {
